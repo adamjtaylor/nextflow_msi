e24ebdd2e39198c6df99b4ef646a8f970ebd3ada
Adam Taylor <adamjtaylor@gmail.com>
2020-08-31 14:47:48 +0100

    Update clustering.m

--- clustering.m
+++ clustering.m
@@ -1,22 +1,26 @@
 function clustering(input_file, distance, k, max_peaks)
   
+  disp('Loading the data')
   % Load the dataacube
   load(input_file);
   
+  disp('Extracting name and filepath')
   [filepath,name,ext] = fileparts(input_file)
   
   % Calculate the mean spectrum
+  disp('Calculate mean spectrum')
   mean_intensity_all = mean(data);
   
   % Make a smaller datacube of the topk peaks
-  
+  disp('Making small datacube')
   [~, top_peaks_idx] = maxk([mean_intensity_all], max_peaks);
   small_data = data(:,top_peaks_idx);
 
+  disp('Running k-means clustering')
   [kmeans_idx, ~, ~ ] = kmeans(small_data, k, 'distance', distance);
 
   %% Make mean spectrum
-
+disp('Calculate cluster mean spectra')
   data_k1 = data(kmeans_idx == 1,:);
   data_k2 = data(kmeans_idx == 2,:);
 
@@ -28,8 +32,8 @@ function clustering(input_file, distance, k, max_peaks)
   clear small_data
 
   % Write into the mat file
-  
-  save([name '.mat'], '-v7.3')
+  disp(['Saving workspace to: ' name '.mat'])
+  save([name '.mat'], '-v7.3');
 
   
  end
